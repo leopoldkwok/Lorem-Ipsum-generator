c5c2d3bb7065821413f88d80c92dfda197ae2b97
leopoldkwok <leopoldkwok@gmail.com>
2021-02-25 22:05:37 +0000

    add handleSubmit functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import data from './data';
+
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello world');
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+
+    if (count > 8) {
+      amount = 8;
+    }
+
+    setText(data.slice(0, amount));
   };
 
   return (
@@ -26,18 +36,9 @@ function App() {
         </button>
       </form>
       <article className='lorem-text'>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. In saepe
-          quasi quisquam ipsam id officia impedit, incidunt facilis, illum
-          blanditiis iure fugiat ratione recusandae aspernatur officiis corrupti
-          sit ad. Alias.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. In saepe
-          quasi quisquam ipsam id officia impedit, incidunt facilis, illum
-          blanditiis iure fugiat ratione recusandae aspernatur officiis corrupti
-          sit ad. Alias.
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
